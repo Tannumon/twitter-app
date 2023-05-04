f5a8f1b947aa861ddc6b1840c1b6307777a2441d
Tanush <tanushsiotia2004@gmail.com>
2023-05-04 18:57:52 -0400

    React Twitter App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Component/Header.js';
+import Body from './Component/Body.js';
+
+function App() {
+  return (
+    <div>
+
+    <div><Header/></div>
+    <div><Body/></div>
+
+    </div>
+  )
+}
+
+
+export default App;
+
